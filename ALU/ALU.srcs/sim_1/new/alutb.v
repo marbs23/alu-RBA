9b80204df9c4f8da3252cbb870c0fbd63f161b2f
marbs23 <martinbonillasarmiento@gmail.com>
2025-10-03 18:03:38 -0500

    Update alutb.v

--- ALU/ALU.srcs/sim_1/new/alutb.v
+++ ALU/ALU.srcs/sim_1/new/alutb.v
@@ -2,28 +2,36 @@
 module tb_alu;
     reg [4:0] a;
     reg [4:0] b;
+    reg [2:0] bshift;
     reg [2:0] ALUControl;
     wire [4:0] Result;
     wire [3:0] ALUFlags;
 
-    alu uut (
+    top topi(
         .a(a),
         .b(b),
         .ALUControl(ALUControl),
+        .bshift(bshift),
         .Result(Result),
         .ALUFlags(ALUFlags)
     );
 
     initial begin
-        a = 3; b = 5; ALUControl = 3'b000; #10;
+        a = 5; b = 7; bshift = 3'b000 ;ALUControl = 3'b011; #10;
 
-        a = 5; b = 5; ALUControl = 3'b001; #10;
+        a = 3; b = 5; bshift = 3'b001 ;ALUControl = 3'b000; #10;
 
-        a = 8; b = 1; ALUControl = 3'b010; #10;
+        a = 5; b = 5; bshift = 3'b010 ;ALUControl = 3'b001; #10;
 
-        a = 5; b = 7; ALUControl = 3'b011; #10;
+        a = 8; b = 1; bshift = 3'b011 ;ALUControl = 3'b010; #10;
+
+        a = 5; b = 7; bshift = 3'b100 ;ALUControl = 3'b011; #10;
         
-        a = 5; b = 7; ALUControl = 3'b100; #10;
+        a = 5; b = 7; bshift = 3'b101 ;ALUControl = 3'b011; #10;
+
+        a = 5; b = 7; bshift = 3'b110 ;ALUControl = 3'b011; #10;
+
+        a = 5; b = 7; bshift = 3'b111 ;ALUControl = 3'b011; #10;
 
         $stop; 
     end
