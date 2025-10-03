d3e3c3e125152f7df5f8bd84c25bff26f0255838
AnnieSld <annemarie.saldarriag@utec.edu.pe>
2025-10-03 17:17:59 -0500

    Update alu.v

--- ALU/ALU.srcs/sources_1/new/alu.v
+++ ALU/ALU.srcs/sources_1/new/alu.v
@@ -30,14 +30,15 @@ module alu(input [4:0] a, b,
 endmodule
 /*
 
+
 module alu(input [4:0] a, b,
-    input [2:0] ALUControl,
+           input [2:0] ALUControl,
     output reg [4:0] Result,
     output wire [3:0] ALUFlags);
     
     wire neg, zero, carry, overflow;
-    wire [3:0] condinvb;
-    wire [4:0] sum;
+  wire [4:0] condinvb;
+  wire [5:0] sum;
     
     assign condinvb = ALUControl[0] ? ~b : b;
     assign sum = a + condinvb + ALUControl[0];
@@ -55,11 +56,12 @@ module alu(input [4:0] a, b,
     assign carry    = (ALUControl[1]==1'b0) & sum[5]; 
     assign overflow = (ALUControl[1]==1'b0) &
                   ~(a[4] ^ b[4] ^ ALUControl[0]) &
-                  (a[4] ^ sum[4]);
+      (a[4] ^ sum[5]);
 
     assign ALUFlags = {neg, zero, carry, overflow};
 
 endmodule
+
 
 
 
